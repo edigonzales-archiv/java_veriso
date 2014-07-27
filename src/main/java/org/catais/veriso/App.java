160a650c28471dc76f54865719dcc4310a5dbb8a
Stefan Ziegler <Stefan Ziegler>
2014-07-27 17:42:54 +0200

    mvn Projekt erstellt

---
+++ src/main/java/org/catais/veriso/App.java
@@ -0,0 +1,13 @@
+package org.catais.veriso;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
